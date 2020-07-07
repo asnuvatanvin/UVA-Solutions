c5aec8098acca3ead2c467c87aece9866c47d2cb
Asnuva Tanvin <asnuva598@gmail.com>
2020-07-07 17:35:14 +0600

    Add files via upload

---
+++ uva-10407-solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+/*............................*/
+
+#define uint uint64_t
+#define mx 100001
+#define ll long long int
+#define ull unsigned long long int
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define mp make_pair
+#define pb push_back
+#define inf INT_MAX
+#define pi acos(-1)
+#define eps 1e-8
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / __gcd(a, b)
+#define input(ara, n)          \
+   for (int i = 0; i < n; i++) \
+   scanf("%d", &ara[i])
+#define inputl(ara, n)                   \
+   for (long long int i = 0; i < n; i++) \
+   scanf("%lld", &ara[i])
+#define pf(a) printf("%d\n", a);
+#define pfl(a) printf("%lld\n", a);
+#define pfi(a) printf("%d ", a);
+#define pfll(a) printf("%lld ", a);
+#define sc(a) scanf("%d", &a)
+#define scc(a, b) scanf("%d %d", &a, &b)
+#define sccdbl(a, b) scanf("%lf %lf", &a, &b)
+#define sccc(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define scccdbl(a, b, c) scanf("%lf %lf %lf", &a, &b, &c)
+#define scl(a) scanf("%lld", &a)
+#define sccl(a, b) scanf("%lld %lld", &a, &b)
+#define scccl(a, b, c) scanf("%lld %lld %lld", &a, &b, &c)
+using namespace std;
+// solution idea https://forthright48.com/uva-10407-simple-division/
+int main()
+{
+   int a;
+   while(sc(a) && a){
+      vector<int>v;
+      v.pb(a);
+      while(sc(a) && a!=0){
+         v.pb(a);
+      }
+      int g = 0;
+      for(int i=1;i<v.size();i++){
+         g = gcd(g,(v[i]-v[i-1]));
+      }
+      if(g<0){
+         g*=-1;
+      }
+      pf(g);
+   }
+   return 0;
+}
